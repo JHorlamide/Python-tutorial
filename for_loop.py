bdceec20a16fee54f252c27a788d56dd46f68af6
Horlamide <olamide.jubril@taxtech.com.ng>
2021-06-19 12:25:47 +0100

    2D list, for loops, list, list methods, nested loops

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+# for item in [1,2,3,4,5]:
+#     print(item)
+
+# for item in [10, 20, 30]:
+#     print(item)
+
+# for item in range(0, 10):
+#     print(item)
+
+prices = [10, 20, 30]
+total = 0
+
+for price in prices:
+    total += price
+print(f'Total: {total}')
---
+++ list.py
@@ -0,0 +1,5 @@
+names = ['Olamide', 'Adeseun', 'Mary', 'Adejoke']
+new_list = names[1:]
+names[0] = 'JHorlamide'
+print(names)
+# print(new_list)
